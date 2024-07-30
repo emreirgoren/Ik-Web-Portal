ca4103f87327983f3fac35becf93c25f9bdeb0b3
emreirgoren <emreirgoren@outlook.com>
2024-07-31 00:01:59 +0300

    İnit

---
+++ ik-portal/src/main/java/com/mcservice/ik_portal/IkPortalApplication.java
@@ -0,0 +1,13 @@
+package com.mcservice.ik_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IkPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IkPortalApplication.class, args);
+	}
+
+}
---
+++ ik-portal/src/test/java/com/mcservice/ik_portal/IkPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcservice.ik_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IkPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
